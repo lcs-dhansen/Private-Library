5f7411017407ba0d7b3a2a254f61396bbff744fc
Devon Hansen <dhansen@lcs.on.ca>
2022-03-25 11:20:39 -0400

    Initial Commit

---
+++ Shared/Private_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Private_LibraryApp.swift
+//  Shared
+//
+//  Created by Devon Kenneth Hansen on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct Private_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
